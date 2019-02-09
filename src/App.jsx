9c760733e31a4e0ac09b1ea01ec76e51ac335d9f
fogelkuba <fogelkuba@gmail.com>
2019-02-09 17:10:36 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import Header from './components/layout/Header';
 import uuid from 'uuid';
+
+import './App.css';
 
 class App extends Component {
     state = {
@@ -60,16 +63,25 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Header />
-                <AddTodo add={this.add} />
-                <Todos
-                    todos={this.state.todos}
-                    remove={this.remove}
-                    toggleCompleted={this.toggleCompleted}
-                />
-                
-            </div>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Route path="/" exact render={props => (
+                        <React.Fragment>
+                            <AddTodo add={this.add} />
+                            <Todos
+                                todos={this.state.todos}
+                                remove={this.remove}
+                                toggleCompleted={this.toggleCompleted}
+                            />
+                        </React.Fragment>
+                    )}/> 
+     
+                    <Route path="/about" component={About}/>
+
+                </div>
+            </Router>  
+  
         );
     }
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header style={headerStyle}>
         <h1>TodoList</h1>
+        <Link style={linkStyle} to="/">Home</Link>
+        <Link style={linkStyle} to="/about">Abouts</Link>
     </header>
   )
 }
@@ -13,5 +16,10 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '15px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  margin: '0 1rem'
 }
 
