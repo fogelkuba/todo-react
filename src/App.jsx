81f4336324df827a20a6a1037ea2ea9bfe3f99a6
fogelkuba <fogelkuba@gmail.com>
2019-02-09 15:47:22 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import uuid from 'uuid';
+
 class App extends Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuid.v4(),
                 title: 'Take out the trash',
                 completed: false
             },
             {
-                id: 2,
+                id: uuid.v4(),
                 title: 'Say hi to mark',
                 completed: true
             },
             {
-                id: 3,
+                id: uuid.v4(),
                 title: 'Meeting at 5pm',
                 completed: false
             },
@@ -42,15 +45,30 @@ class App extends Component {
         
     }
 
+    add = (title) => {
+        this.setState({
+            todos: [
+                ...this.state.todos, 
+                {
+                    id: uuid.v4(),
+                    title: title,
+                    completed: false
+                }
+            ]
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <Header />
+                <AddTodo add={this.add} />
                 <Todos
                     todos={this.state.todos}
                     remove={this.remove}
                     toggleCompleted={this.toggleCompleted}
                 />
+                
             </div>
         );
     }
